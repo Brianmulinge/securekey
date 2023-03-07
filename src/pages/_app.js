0115532de3386a4feaf81e375683d53cf4ed5584
Brianjaden <brianjaden45@gmail.com>
2023-03-07 06:38:10 -0600

    migrating to typescript

--- src/pages/_app.js
+++
@@ -1,9 +0,0 @@
-import "../styles/globals.css";
-
-export default function App({ Component, pageProps }) {
-  return (
-    <main className="font-sans">
-      <Component {...pageProps} />
-    </main>
-  );
-}
---
+++ src/pages/_app.tsx
@@ -0,0 +1,16 @@
+import "../styles/globals.css";
+import { AppProps } from "next/app";
+
+import { Inter } from "@next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <main className={inter.className}>
+      <Component {...pageProps} />
+    </main>
+  );
+}
+
+export default App;
