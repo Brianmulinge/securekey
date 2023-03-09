d0cdb9a8bb8fab91acb266f600a4a16cdbc69373
Brianjaden <brianjaden45@gmail.com>
2023-03-08 23:14:06 -0600

    completed project with all the features

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,26 @@
+import Image from "next/image";
 import Link from "next/link";
+import { AiFillGithub } from "react-icons/ai";
+import pic from "../assets/favicon.ico";
 
 export default function Header() {
   return (
-    <section className="h-14 flex px-6 items-center justify-between font-semibold border-b">
-      <h1 className="lg:text-3xl md:text-2xl sm:text-xl">SecureKey</h1>
-      <div className="flex space-x-4">
-        <Link href="https://github.com/Brianmulinge/securekey">
-          <h1>Star On Github</h1>
+    <header className="h-14 flex px-4 items-center justify-between font-semibold border-b">
+      <div className="flex space-x-1 items-center">
+        <Image alt="securekey" className="h-6 w-6" src={pic} />
+        <h1 className="text-lg lg:text-3xl md:text-2xl sm:text-xl">
+          SecureKey
+        </h1>
+      </div>
+      <div className="flex">
+        <Link
+          className="flex space-x-1 items-center"
+          href="https://github.com/Brianmulinge"
+        >
+          <AiFillGithub className="h-8 w-8" />
+          <h1>Github</h1>
         </Link>
       </div>
-    </section>
+    </header>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,44 +1,141 @@
+import Checkbox from "../components/Checkbox";
+import Footer from "../components/Footer";
 import Header from "../components/Header";
+import { useState } from "react";
+
+export const checkboxes = [
+  { id: 1, option: "Letters" },
+  { id: 2, option: "Numbers" },
+  { id: 3, option: "Symbols" },
+];
 
 export default function Home() {
+  const [loading, setLoading] = useState<boolean>(false);
+  const [length, setLength] = useState<number>(0);
+  const [characterSets, setCharacterSets] = useState<boolean[]>([]);
+  const [generatedPassword, setGeneratedPassword] = useState<string>("");
+
+  const handleLength = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setLength(parseInt(e.target.value));
+  };
+
+  const handleCharacterSets = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const id = parseInt(e.target.id.split("-")[1]);
+    const newCharacterSets = [...characterSets];
+    newCharacterSets[id - 1] = e.target.checked;
+    setCharacterSets(newCharacterSets);
+  };
+
+  const generatePassword = () => {
+    const letters = "abcdefghijklmnopqrstuvwxyz";
+    const numbers = "0123456789";
+    const symbols = "!@#$%^&*_+";
+
+    let characterSet = "";
+    if (characterSets[0]) {
+      characterSet += letters;
+    }
+    if (characterSets[1]) {
+      characterSet += numbers;
+    }
+    if (characterSets[2]) {
+      characterSet += symbols;
+    }
+
+    let generatedPassword = "";
+    for (let i = 0; i < length; i++) {
+      generatedPassword += characterSet.charAt(
+        Math.floor(Math.random() * characterSet.length)
+      );
+    }
+
+    setGeneratedPassword(generatedPassword);
+
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  };
+
+  const copyPassword = () => {
+    navigator.clipboard.writeText(generatedPassword);
+  };
+
+  const handleGenerate = () => {
+    if (length === 0) {
+      alert("Please select the length of the password");
+    } else if (characterSets.length === 0) {
+      alert("Please select the character sets for the password");
+    } else {
+      generatePassword();
+    }
+  };
+
+  const handleCopy = () => {
+    if (generatedPassword === "") {
+      alert("Please generate a password first");
+    } else {
+      copyPassword();
+      alert("Password copied to clipboard");
+    }
+  };
+
   return (
-    <div className="flex flex-col">
-      <Header />
-      <div className="flex flex-col text-center items-center content-center justify-center px-4">
-        <h1 className="sm:text-6xl text-4xl max-w-2xl font-bold text-black">
-          Create the password of your choice
-        </h1>
-        <div className="items-start">
-          <div className="space-y-2">
-            <h1>1.Select the length you want your password to be:</h1>
-            <select className="select select-bordered w-full max-w-xs">
-              <option disabled selected>
-                Select the length
-              </option>
-              <option>10</option>
-              <option>15</option>
-              <option>20</option>
-            </select>
-          </div>
-          <div>
-            <h1>2.Select the character sets for your password:</h1>
-            <div className="form-control">
-              <label className="label cursor-pointer">
-                <span className="label-text">Remember me</span>
-                <input type="checkbox" className="checkbox" />
-              </label>
+    <div className="h-screen w-full flex flex-col justify-between">
+      <div>
+        <Header />
+      </div>
+      <div className="px-4">
+        <div className="flex justify-center">
+          <h1 className="text-center sm:text-6xl text-4xl max-w-2xl font-bold text-black">
+            Create the password of your choice
+          </h1>
+        </div>
+        <div className="flex flex-col items-center justify-center">
+          <div className="items-start">
+            <div className="flex flex-col items-start py-2">
+              <h1 className="py-2">1.Select the length of the Password:</h1>
+              <select
+                onChange={handleLength}
+                className="select select-bordered w-full max-w-xs"
+              >
+                <option>Select the length</option>
+                <option>10</option>
+                <option>15</option>
+                <option>20</option>
+                <option>25</option>
+              </select>
             </div>
-          </div>
-          <div className="space-y-4 py-2">
-            <h1>3.Click the generate button to generate your password:</h1>
-            <button className="btn btn-primary">Generate Password</button>
-          </div>
-          <div className="space-y-4 py-2">
-            <h1>4.Copy your password to clipboard:</h1>
-            <button className="btn btn-primary">Copy Password</button>
+            <div className="flex flex-col items-start">
+              <h1 className="py-2">2.The character sets for your password:</h1>
+              <div className="flex space-x-2 pb-2">
+                {checkboxes.map((checkbox) => (
+                  <Checkbox
+                    handleCharacterSets={handleCharacterSets}
+                    key={checkbox.id}
+                    id={checkbox.id}
+                    option={checkbox.option}
+                  />
+                ))}
+              </div>
+            </div>
+            <div className="pt-4">
+              <div className="flex items-start justify-center space-x-2">
+                <button onClick={handleGenerate} className="btn btn-primary">
+                  Generate
+                </button>
+                <button onClick={handleCopy} className="btn btn-primary">
+                  Copy Password
+                </button>
+              </div>
+              <h1 className="text-center pt-10">
+                {loading ? "Loading..." : generatedPassword}
+              </h1>
+            </div>
           </div>
         </div>
       </div>
+      <Footer />
     </div>
   );
 }
